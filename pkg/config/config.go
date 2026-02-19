3a73141052e429d74962c05538ad2598f0fccb88
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:02:40 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -33,6 +33,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the Duo API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 
 	ConfigurationFields = []field.SchemaField{
